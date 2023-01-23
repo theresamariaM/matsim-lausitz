6ad73e8f75f2ab383fd440c612973e697bd49666
Christian Rakow <rakow@vsp.tu-berlin.de>
2023-01-23 16:09:22 +0100

    remove logger

--- src/main/java/org/matsim/run/analysis/DownloadCommuterStatistic.java
+++ src/main/java/org/matsim/run/analysis/DownloadCommuterStatistic.java
@@ -95,8 +95,6 @@ public class DownloadCommuterStatistic implements MATSimAppCommand {
 
 				log.info("Parsed {} relations", parsed.size());
 
-				System.out.println(parsed);
-
 				results.addAll(parsed);
 			}
 		}

c9d824b03fc417ef75cd4434b0a8cc00204eb072
rakow <rakow@vsp.tu-berlin.de>
2024-03-19 18:00:11 +0100

    update v1.0 tests

--- src/test/java/org/matsim/run/RunIntegrationTest.java
+++ src/test/java/org/matsim/run/RunIntegrationTest.java
@@ -3,7 +3,10 @@ package org.matsim.run;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.matsim.application.MATSimApplication;
+import org.matsim.core.events.EventsUtils;
 import org.matsim.testcases.MatsimTestUtils;
+
+import java.io.File;
 
 public class RunIntegrationTest {
 
@@ -16,8 +19,15 @@ public class RunIntegrationTest {
 		assert MATSimApplication.execute(LausitzScenario.class,
 			"--1pct",
 			"--iterations", "1",
+			"--config:plans.inputPlansFile", "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/lausitz/input/v1.0/lausitz-v1.0-1pct.plans-initial.xml.gz",
 			"--output", utils.getOutputDirectory(),
 			"--config:controller.overwriteFiles=deleteDirectoryIfExists") == 0 : "Must return non error code";
+
+
+		EventsUtils.assertEqualEventsFingerprint(
+			new File(utils.getOutputDirectory(), "lausitz-1pct.output_events.xml.gz"),
+			new File(utils.getClassInputDirectory(), "lausitz-1pct.output_events.fp.zst").toString()
+		);
 
 	}
 }
